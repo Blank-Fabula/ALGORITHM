2241f22d787677729bf0b5219106dbe5fa404cc8
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-09 08:50:37 +0900

    Update 최대공약수 & 최소공배수.cc

--- C++/최대공약수 & 최소공배수.cc
+++ C++/최대공약수 & 최소공배수.cc
@@ -1,9 +1,9 @@
-//최대공약수;
+//최대공약수(Greatest Common Divisor);
 int gcd(int A, int B) {
 	if (B == 0) return A;
 	return gcd(B, A % B);
 }
-//최소공배수;
+//최소공배수(Least Common Multiple);
 int lcm(int A, int B) {
 	return A * B / gcd(A, B);
 }
