ed6f2105fe30c54609cf869956f2a4837a9dfd65
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-09 12:16:26 +0900

    Update 1부터 N까지 합.cc

--- C++/1부터 N까지 합.cc
+++ C++/1부터 N까지 합.cc
@@ -1,6 +1,12 @@
-int sum(int N){
+//1부터 N까지 하나씩 더하는 방법;
+int sum(int N) {
 	int total = 0;
-	for(int i = 1; i <= N; i++){
+	for (int i = 1; i <= N; i++) {
 		total += i;
 	}
+return total; }
+//수학식을 이용하는 방법;
+int sum(int N) {
+	int total = 0;
+	total = N * (1 + N) / 2;
 return total; }
