b5d56c9b7146467db5c375006bcdecd2f22a4a5b
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-10 20:54:27 +0900

    Update 순차탐색.cc

--- C++/순차탐색.cc
+++ C++/순차탐색.cc
@@ -1,10 +1,11 @@
-//순차탐색(sequential_search)
+//[배열에 존재 하지 않을 경우 -1을 반환];
+//순차탐색(sequential_search);
 int seq_search(int size, vector<int>& list, int target) {
 	for (int i = 0; i < size; i++) {
 		if (list[i] == target) return i;
 	}
 return -1; }
-//오름차순으로 정렬된 배열에서의 순차탐색(sequential_search_sorted)
+//오름차순으로 정렬된 배열에서의 순차탐색(sequential_search_sorted);
 int seq_search_srt(int size, vector<int>& list, int target) {
 	for (int i = 0; i < size; i++) {
 		if (list[i] == target) return i;
